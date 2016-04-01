753f6185913175bc77d5c9d0222fd6bd12c1549f
Geoffroy Planquart <geoffroy@planquart.fr>
2016-04-02 00:14:57 +0200

    Silencing bundle output

--- etc/rbenv.d/which/user-gems.bash
+++ etc/rbenv.d/which/user-gems.bash
@@ -2,7 +2,7 @@ source "${BASH_SOURCE%/*}/../../../share/rbenv-user-gems.sh"
 
 if gemdir="$(user_gems_dir "$RBENV_VERSION")"; then
   if [ -x "$RBENV_ROOT/versions/$RBENV_VERSION/bin/bundle" ]; then
-    $RBENV_ROOT/versions/$RBENV_VERSION/bin/bundle config path "$(user_gems_dir "$RBENV_VERSION")"
+    $RBENV_ROOT/versions/$RBENV_VERSION/bin/bundle config path "$(user_gems_dir "$RBENV_VERSION")" > /dev/null 2>&1
   fi
   if [ ! -x "$RBENV_COMMAND_PATH" ]; then
     RBENV_COMMAND_PATH="${gemdir}/bin/${RBENV_COMMAND}"
