d9d38fef24c181c4fea7afc599b3930f6d4e565c
Geoffroy Planquart <geoffroy@planquart.fr>
2016-04-01 23:17:15 +0200

    Override bundle config path on rbenv which

--- etc/rbenv.d/which/user-gems.bash
+++ etc/rbenv.d/which/user-gems.bash
@@ -1,7 +1,8 @@
-if [ ! -x "$RBENV_COMMAND_PATH" ]; then
-  source "${BASH_SOURCE%/*}/../../../share/rbenv-user-gems.sh"
+source "${BASH_SOURCE%/*}/../../../share/rbenv-user-gems.sh"
 
-  if gemdir="$(user_gems_dir "$RBENV_VERSION")"; then
+if gemdir="$(user_gems_dir "$RBENV_VERSION")"; then
+  $RBENV_ROOT/versions/$RBENV_VERSION/bin/bundle config path "$(user_gems_dir "$RBENV_VERSION")"
+  if [ ! -x "$RBENV_COMMAND_PATH" ]; then
     RBENV_COMMAND_PATH="${gemdir}/bin/${RBENV_COMMAND}"
   fi
 fi
