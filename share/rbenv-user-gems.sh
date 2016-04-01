323b3528b83d1cbf668c415f5a114e050a6358f0
Geoffroy Planquart <geoffroy@planquart.fr>
2016-04-01 20:19:06 +0200

    Add ruby 2.3.x support

--- share/rbenv-user-gems.sh
+++ share/rbenv-user-gems.sh
@@ -16,6 +16,7 @@ user_gems_dir() {
   2.0.*   ) gemdir="ruby/2.0.0" ;;
   2.1.*   ) gemdir="ruby/2.1.0" ;;
   2.2.*   ) gemdir="ruby/2.2.0" ;;
+  2.3.*   ) gemdir="ruby/2.3.0" ;;
   jruby-* ) gemdir="jruby/1.9" ;;
   rbx-2.* ) gemdir="rbx/2.1" ;;
   esac
