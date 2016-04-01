ead420a787cc4bd3abcbe30a495f6dfa183df884
Geoffroy Planquart <geoffroy@planquart.fr>
2016-04-02 00:03:01 +0200

    Verifying bundle existence before call

--- etc/rbenv.d/which/user-gems.bash
+++ etc/rbenv.d/which/user-gems.bash
@@ -1,7 +1,9 @@
 source "${BASH_SOURCE%/*}/../../../share/rbenv-user-gems.sh"
 
 if gemdir="$(user_gems_dir "$RBENV_VERSION")"; then
-  $RBENV_ROOT/versions/$RBENV_VERSION/bin/bundle config path "$(user_gems_dir "$RBENV_VERSION")"
+  if [ -x "$RBENV_ROOT/versions/$RBENV_VERSION/bin/bundle" ]; then
+    $RBENV_ROOT/versions/$RBENV_VERSION/bin/bundle config path "$(user_gems_dir "$RBENV_VERSION")"
+  fi
   if [ ! -x "$RBENV_COMMAND_PATH" ]; then
     RBENV_COMMAND_PATH="${gemdir}/bin/${RBENV_COMMAND}"
   fi
